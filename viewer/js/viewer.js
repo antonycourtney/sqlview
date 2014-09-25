1cea12a544a7fbbaf2a59aa46a923f8b273dc70e
Antony Courtney <antony.courtney@gmail.com>
2014-09-25 13:56:03 -0700

    full history, acceptable formatting

--- viewer/js/viewer.js
+++ viewer/js/viewer.js
@@ -11,15 +11,57 @@ var React = require('react');
 var SqlQuery = require('./components/sqlquery.js');
 var DataGrid = require('./components/datagrid.js');
 
-console.log("Hello, world!");
-
-var historyUrl = "/getHistory";
-
 var gridOptions = {
     enableCellNavigation: true,
     enableColumnReorder: false
 };        
 
+var QueryLogViewer = React.createClass(
+ {
+    getInitialState: function() {
+        return {
+            queryLog: []
+        };
+    },
+    
+    render: function() {
+        var rows = [];
+        var queryLog = this.state.queryLog;
+        for (var i = 0; i < queryLog.length; i++) {
+            var entry = queryLog[i];
+            var columnNames = entry.result.columnNames;
+            var gridData = entry.result.data;        
+            rows.push(
+                <tr key={i} className="query-log-row">
+                    <td className="history-index">{"[" + i + "]: "}</td>
+                    <td className="query-log-entry">
+                        <SqlQuery query={entry.query} />
+                        <DataGrid columnNames={columnNames} data={gridData} options={gridOptions} />
+                    </td>
+                </tr>
+            );
+        }
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>{rows}</tbody>
+            </table>
+            );
+        return <div className="sql-query"><pre>{sql}</pre></div>;
+    }
+ });
+
+var logViewer = React.renderComponent( 
+    <QueryLogViewer />,
+    document.getElementById("content")
+);
+
+var historyUrl = "/getHistory";
 var hp = Q($.ajax({
     url: historyUrl,
     type: "GET"
@@ -27,18 +69,11 @@ var hp = Q($.ajax({
 
 hp.then(function (data) {
     console.log("Got history: ", data);
-    if (data.history.length > 0 ) {
-        var entry = data.history[0];
-        var columnNames = entry.result.columnNames;
-        var gridData = entry.result.data;        
-        React.renderComponent(
-            <div>
-                <SqlQuery query={entry.query} />
-                <DataGrid columnNames={columnNames} data={gridData} options={gridOptions} />
-            </div>,
-            document.getElementById("content")
-        );
-    }
+    console.log("logViewer: ", logViewer );
+    logViewer.setState({ queryLog: data.history } );
+    console.log("Set state on log viewer...");    
 }).catch(function (e) {
     console.error("caught unhandled promise exception: ", e.stack, e);
 });
+
+
